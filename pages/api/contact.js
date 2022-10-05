ab65afe5d0d58fd663ff66125a31bad06a5fdc8a
Juan <thecatbox@protonmail.com>
2022-10-05 12:39:43 -0300

    contact configs

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,16 +1,28 @@
-require('dotenv').config()
-const PASSWORD = process.env.password
-
+require("dotenv").config();
+const PASSWORD = process.env.PASSWORD;
+const OWNEREMAIL = process.env.OWNEREMAIL;
 export default function (req, res) {
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-          user: 'demo@demo.gmail',
-          pass: 'password',
-        },
-        secure: true,
-      });
-    console.log(req.body)
-  }
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "inakilambrechtsadmn@gmail.com",
+      pass: PASSWORD,
+    },
+    secure: true,
+  });
+  const mailData = {
+    from: "inakilambrechtsadmn@gmail.com",
+    to: OWNEREMAIL,
+    subject: `Message from ${req.body.message.name}`,
+    text: req.body.message.message,
+    sender: req.body.message.email,
+    html: `<div><p>from ${req.body.message.email}: </p><p>${req.body.message.message}</p></div>`,
+  };
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+  res.status(200);
+}
