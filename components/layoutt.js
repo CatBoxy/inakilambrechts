ff76c22d277bc6d4458ff1b0cf7e0b73537690ca
Juan <thecatbox@protonmail.com>
2022-12-04 22:10:33 -0300

    todo5

--- components/layoutt.js
+++ components/layoutt.js
@@ -10,6 +10,7 @@ export default function Layout({ preview, children }) {
         <Alert preview={preview} />
         <main>{children}</main>
         <div></div>
+        <div></div>
       </div>
       <Footer />
     </>
